c0e9fef2560119bc2293979edf098e3db25708de
A. Parkos <65038075+thuotdwz@users.noreply.github.com>
2020-05-20 01:48:25 -0700

    Remove macOS 10_15 iOS 13 platforms
    This is not necessary and causes error in SQLiteKit:
    The package product 'SQLiteNIO' requires minimum platform version 13.0 for the iOS platform, but this target supports 8.0

--- Package.swift
+++ Package.swift
@@ -3,10 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "sqlite-nio",
-    platforms: [
-       .macOS(.v10_15),
-       .iOS(.v13)
-    ],
     products: [
         .library(name: "SQLiteNIO", targets: ["SQLiteNIO"]),
     ],
