31641c59f3686b938d57128019275e4344ec1cad
Austin Payne <austin@passivelogic.com>
2022-11-15 15:08:04 -0700

    Adjust CSQLite compile options to Ubuntu 22.04

--- Package.swift
+++ Package.swift
@@ -29,85 +29,10 @@ let package = Package(
     ]
 )
 
-#if os(macOS)
-// Derived from sqlite3 version 3.37.0 2021-12-09 01:34:53 9ff244ce0739f8ee52a3e9671adb4ee54c83c640b02e3f9d185fd2f9a179aapl
-// compiled with clang-13.1.6
-// on platform macOS 12.6 (21G115)
-let cSQLiteSettings: [CSetting] = [
-    .define("SQLITE_ATOMIC_INTRINSICS", to: "1"),
-    .define("SQLITE_DEFAULT_CACHE_SIZE", to: "2000"),
-    .define("SQLITE_DEFAULT_FILE_FORMAT", to: "4"),
-    .define("SQLITE_DEFAULT_JOURNAL_SIZE_LIMIT", to: "32768"),
-    .define("SQLITE_DEFAULT_LOOKASIDE", to: "1200,102"),
-    .define("SQLITE_DEFAULT_MEMSTATUS", to: "0"),
-    .define("SQLITE_DEFAULT_MMAP_SIZE", to: "0"),
-    .define("SQLITE_DEFAULT_PAGE_SIZE", to: "4096"),
-    .define("SQLITE_DEFAULT_PCACHE_INITSZ", to: "20"),
-    .define("SQLITE_DEFAULT_SECTOR_SIZE", to: "4096"),
-    .define("SQLITE_DEFAULT_SYNCHRONOUS", to: "2"),
-    .define("SQLITE_DEFAULT_WAL_AUTOCHECKPOINT", to: "1000"),
-    .define("SQLITE_DEFAULT_WAL_SYNCHRONOUS", to: "1"),
-    .define("SQLITE_DEFAULT_WORKER_THREADS", to: "0"),
-    .define("SQLITE_ENABLE_LOCKING_STYLE", to: "1"),
-    .define("SQLITE_MALLOC_SOFT_LIMIT", to: "1024"),
-    .define("SQLITE_MAX_ATTACHED", to: "10"),
-    .define("SQLITE_MAX_COLUMN", to: "2000"),
-    .define("SQLITE_MAX_COMPOUND_SELECT", to: "500"),
-    .define("SQLITE_MAX_DEFAULT_PAGE_SIZE", to: "8192"),
-    .define("SQLITE_MAX_EXPR_DEPTH", to: "1000"),
-    .define("SQLITE_MAX_FUNCTION_ARG", to: "127"),
-    .define("SQLITE_MAX_LENGTH", to: "2147483645"),
-    .define("SQLITE_MAX_LIKE_PATTERN_LENGTH", to: "50000"),
-    .define("SQLITE_MAX_MMAP_SIZE", to: "1073741824"),
-    .define("SQLITE_MAX_PAGE_COUNT", to: "1073741823"),
-    .define("SQLITE_MAX_PAGE_SIZE", to: "65536"),
-    .define("SQLITE_MAX_SQL_LENGTH", to: "1000000000"),
-    .define("SQLITE_MAX_TRIGGER_DEPTH", to: "1000"),
-    .define("SQLITE_MAX_VARIABLE_NUMBER", to: "500000"),
-    .define("SQLITE_MAX_VDBE_OP", to: "250000000"),
-    .define("SQLITE_MAX_WORKER_THREADS", to: "8"),
-    .define("SQLITE_STMTJRNL_SPILL", to: "131072"),
-    .define("SQLITE_TEMP_STORE", to: "1"),
-    .define("SQLITE_THREADSAFE", to: "2"),
-    .define("SQLITE_BUG_COMPATIBLE_20160819"),
-    .define("SQLITE_DEFAULT_AUTOVACUUM"),
-    .define("SQLITE_DEFAULT_CKPTFULLFSYNC"),
-    .define("SQLITE_DEFAULT_RECURSIVE_TRIGGERS"),
-    .define("SQLITE_ENABLE_API_ARMOR"),
-    .define("SQLITE_ENABLE_BYTECODE_VTAB"),
-    .define("SQLITE_ENABLE_COLUMN_METADATA"),
-    .define("SQLITE_ENABLE_DBPAGE_VTAB"),
-    .define("SQLITE_ENABLE_DBSTAT_VTAB"),
-    .define("SQLITE_ENABLE_EXPLAIN_COMMENTS"),
-    .define("SQLITE_ENABLE_FTS3"),
-    .define("SQLITE_ENABLE_FTS3_PARENTHESIS"),
-    .define("SQLITE_ENABLE_FTS3_TOKENIZER"),
-    .define("SQLITE_ENABLE_FTS4"),
-    .define("SQLITE_ENABLE_FTS5"),
-    .define("SQLITE_ENABLE_JSON1"),
-    .define("SQLITE_ENABLE_NORMALIZE"),
-    .define("SQLITE_ENABLE_PREUPDATE_HOOK"),
-    .define("SQLITE_ENABLE_RTREE"),
-    .define("SQLITE_ENABLE_SESSION"),
-    .define("SQLITE_ENABLE_SNAPSHOT"),
-    .define("SQLITE_ENABLE_STMT_SCANSTATUS"),
-    .define("SQLITE_ENABLE_UNKNOWN_SQL_FUNCTION"),
-    .define("SQLITE_ENABLE_UPDATE_DELETE_LIMIT"),
-    .define("SQLITE_HAVE_ISNAN"),
-    .define("SQLITE_MUTEX_UNFAIR"),
-    .define("SQLITE_OMIT_AUTORESET"),
-    .define("SQLITE_OMIT_LOAD_EXTENSION"),
-    .define("SQLITE_SYSTEM_MALLOC"),
-    .define("SQLITE_USE_URI"),
-]
-#else
-// Derived from sqlite3 version 3.31.1 2020-01-27 19:55:54 3bfa9cc97da10598521b342961df8f5f68c7388fa117345eeb516eaa837balt1
-// compiled with gcc-9.4.0
-// on platform Ubuntu 20.04.5 LTS (Focal Fossa)
-let cSQLiteSettings: [CSetting] = [
-    .define("SQLITE_MAX_SCHEMA_RETRY", to: "25"),
-    .define("SQLITE_MAX_VARIABLE_NUMBER", to: "250000"),
-    .define("SQLITE_THREADSAFE", to: "1"),
+// Derived from sqlite3 version 3.37.2 2022-01-06 13:25:41 872ba256cbf61d9290b571c0e6d82a20c224ca3ad82971edc46b29818d5dalt1
+// compiled with gcc-11.3.0
+// on platform Ubuntu 22.04.1 LTS (Jammy Jellyfish)
+var cSQLiteSettings: [CSetting] = [
     .define("SQLITE_ENABLE_COLUMN_METADATA"),
     .define("SQLITE_ENABLE_DBSTAT_VTAB"),
     .define("SQLITE_ENABLE_FTS3"),
@@ -116,7 +41,6 @@ let cSQLiteSettings: [CSetting] = [
     .define("SQLITE_ENABLE_FTS4"),
     .define("SQLITE_ENABLE_FTS5"),
     .define("SQLITE_ENABLE_JSON1"),
-    .define("SQLITE_ENABLE_LOAD_EXTENSION"),
     .define("SQLITE_ENABLE_PREUPDATE_HOOK"),
     .define("SQLITE_ENABLE_RTREE"),
     .define("SQLITE_ENABLE_SESSION"),
@@ -126,11 +50,33 @@ let cSQLiteSettings: [CSetting] = [
     .define("SQLITE_ENABLE_UPDATE_DELETE_LIMIT"),
     .define("SQLITE_HAVE_ISNAN"),
     .define("SQLITE_LIKE_DOESNT_MATCH_BLOBS"),
+    .define("SQLITE_MUTEX_PTHREADS"),
+    .define("SQLITE_OMIT_LOAD_EXTENSION"),
     .define("SQLITE_OMIT_LOOKASIDE"),
     .define("SQLITE_SECURE_DELETE"),
-    .define("SQLITE_SOUNDEX"),
+    .define("SQLITE_SYSTEM_MALLOC"),
     .define("SQLITE_USE_URI"),
 ]
+
+#if os(macOS)
+// Derived from sqlite3 version 3.37.0 2021-12-09 01:34:53 9ff244ce0739f8ee52a3e9671adb4ee54c83c640b02e3f9d185fd2f9a179aapl
+// compiled with clang-13.1.6
+// on platform macOS 12.6 (21G115)
+cSQLiteSettings.append(contentsOf: [
+    .define("SQLITE_DEFAULT_CACHE_SIZE", to: "2000"),
+    .define("SQLITE_DEFAULT_JOURNAL_SIZE_LIMIT", to: "32768"),
+    .define("SQLITE_DEFAULT_LOOKASIDE", to: "1200,102"),
+    .define("SQLITE_DEFAULT_MEMSTATUS", to: "0"),
+    .define("SQLITE_DEFAULT_MMAP_SIZE", to: "0"),
+    .define("SQLITE_DEFAULT_PAGE_SIZE", to: "4096"),
+    .define("SQLITE_DEFAULT_PCACHE_INITSZ", to: "20"),
+    .define("SQLITE_DEFAULT_SYNCHRONOUS", to: "2"),
+    .define("SQLITE_DEFAULT_WAL_AUTOCHECKPOINT", to: "1000"),
+    .define("SQLITE_DEFAULT_WAL_SYNCHRONOUS", to: "1"),
+    .define("SQLITE_DEFAULT_WORKER_THREADS", to: "0"),
+    .define("SQLITE_ENABLE_LOCKING_STYLE", to: "1"),
+    .define("SQLITE_MAX_MMAP_SIZE", to: "1073741824"),
+])
 #endif
 
 package.targets.append(
