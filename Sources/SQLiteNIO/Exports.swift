55f262c60882040e167db8280ab2c7fc0a387524
Tim <0xtimc@gmail.com>
2023-02-22 10:57:05 +0000

    Add DocC wrappers to exports

--- Sources/SQLiteNIO/Exports.swift
+++ Sources/SQLiteNIO/Exports.swift
@@ -1,5 +1,9 @@
+#if !BUILDING_DOCC
+
 @_exported import struct NIO.ByteBuffer
 @_exported import class NIO.NIOThreadPool
 @_exported import protocol NIO.EventLoop
 @_exported import protocol NIO.EventLoopGroup
 @_exported import class NIO.MultiThreadedEventLoopGroup
+
+#endif
